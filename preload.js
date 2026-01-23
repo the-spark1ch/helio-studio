8b2f58ec3cb920887b28555d2bddb71721cbf7c0
the-spark1ch <online.spark1ch@gmail.com>
2026-01-23 20:22:59 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,35 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("api", {
+function assertString(v, name) {
+  if (typeof v !== "string" || v.length === 0) {
+    throw new Error(`${name} must be a non-empty string`);
+  }
+}
+
+const api = {
   openFolder: () => ipcRenderer.invoke("dialog:openFolder"),
   openFile: () => ipcRenderer.invoke("dialog:openFile"),
-  readFile: (p) => ipcRenderer.invoke("fs:readFile", p),
-  writeFile: (p, c) => ipcRenderer.invoke("fs:writeFile", p, c),
-  listDir: (p) => ipcRenderer.invoke("fs:listDir", p)
-});
+
+  readFile: (p) => {
+    assertString(p, "path");
+    return ipcRenderer.invoke("fs:readFile", p);
+  },
+
+  writeFile: (p, content) => {
+    assertString(p, "path");
+    if (typeof content !== "string") throw new Error("content must be a string");
+    return ipcRenderer.invoke("fs:writeFile", p, content);
+  },
+
+  listDir: (p) => {
+    assertString(p, "path");
+    return ipcRenderer.invoke("fs:listDir", p);
+  },
+
+  openExternal: (url) => {
+    assertString(url, "url");
+    return ipcRenderer.invoke("shell:openExternal", url);
+  }
+};
+
+contextBridge.exposeInMainWorld("api", Object.freeze(api));
