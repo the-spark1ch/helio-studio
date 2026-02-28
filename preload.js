403ced9ec95bfd88fee624d3df2f98da5aa26d69
the-spark1ch <online.spark1ch@gmail.com>
2026-02-28 13:13:51 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -6,6 +6,12 @@ function assertString(v, name) {
   }
 }
 
+function assertRecentItem(item) {
+  if (!item || typeof item !== "object") throw new Error("item must be an object");
+  if (item.type !== "file" && item.type !== "folder") throw new Error("item.type must be 'file' or 'folder'");
+  assertString(item.path, "item.path");
+}
+
 const api = {
   openFolder: () => ipcRenderer.invoke("dialog:openFolder"),
   openFile: () => ipcRenderer.invoke("dialog:openFile"),
@@ -29,7 +35,20 @@ const api = {
   openExternal: (url) => {
     assertString(url, "url");
     return ipcRenderer.invoke("shell:openExternal", url);
-  }
+  },
+
+  recent: Object.freeze({
+    get: () => ipcRenderer.invoke("recent:get"),
+    clear: () => ipcRenderer.invoke("recent:clear"),
+    add: (item) => {
+      assertRecentItem(item);
+      return ipcRenderer.invoke("recent:add", item);
+    },
+    open: (item) => {
+      assertRecentItem(item);
+      return ipcRenderer.invoke("recent:open", item);
+    }
+  })
 };
 
 contextBridge.exposeInMainWorld("api", Object.freeze(api));
