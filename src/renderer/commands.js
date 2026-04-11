2e96cc026321660ff84939fb72571230b1c44112
the-spark1ch <online.spark1ch@gmail.com>
2026-04-11 11:35:59 +0300

    Create commands.js

---
+++ src/renderer/commands.js
@@ -0,0 +1,17 @@
+export function getCommands(actions) {
+  return [
+    { id: "save", title: "Save File", shortcut: "Ctrl+S", action: actions.saveCurrentFile },
+    { id: "open-file", title: "Open File...", shortcut: "Ctrl+O", action: actions.openFileFlow },
+    { id: "open-folder", title: "Open Folder...", shortcut: "Ctrl+Shift+O", action: actions.openFolderFlow },
+    { id: "close-tab", title: "Close Current Tab", shortcut: "Ctrl+W", action: actions.requestCloseCurrentTab },
+    { id: "close-all-tabs", title: "Close All Tabs", action: actions.closeAllTabs },
+    { id: "next-tab", title: "Next Tab", shortcut: "Ctrl+Tab", action: actions.nextTab },
+    { id: "prev-tab", title: "Previous Tab", shortcut: "Ctrl+Shift+Tab", action: actions.prevTab },
+    { id: "settings", title: "Open Settings", action: actions.openSettings },
+    { id: "toggle-theme", title: "Toggle Theme", action: actions.toggleTheme },
+    { id: "font-increase", title: "Increase Font Size", shortcut: "Ctrl++", action: actions.increaseFontSize },
+    { id: "font-decrease", title: "Decrease Font Size", shortcut: "Ctrl+-", action: actions.decreaseFontSize },
+    { id: "font-reset", title: "Reset Font Size", action: actions.resetFontSize },
+    { id: "goto-line", title: "Go to Line...", shortcut: "Ctrl+G", action: actions.gotoLine }
+  ];
+}
